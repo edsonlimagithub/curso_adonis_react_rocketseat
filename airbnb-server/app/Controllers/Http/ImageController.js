6a185aafdf63752986a76221f7b334b4e988f4f5
Edson Lima <edsonlima2178@gmail.com>
2019-01-15 19:58:59 -0300

    first comit

---
+++ airbnb-server/app/Controllers/Http/ImageController.js
@@ -0,0 +1,43 @@
+'use restrict'
+
+// import { promises } from "fs";
+// import { prototype } from "stream";
+
+const Helpers = use('Helpers')
+// const Image = use('App/Models/Image')
+const Property = use('App/Models/Property')
+
+
+
+class ImageController {
+
+  async store ({ params, request }){
+    const property = await Property.findOrFail(params.id)
+
+    const images = request.file('image', {
+      types: ['image'],
+      size: '2mb'
+    })
+
+    await images.moveAll(Helpers.tmpPath('uploads'), file => ({
+      name: `${Date.now()}-${file.clientName}`
+    }))
+
+    if(!images.moveAll()){
+      return images.errors()
+    }
+
+    await Promise.all(
+      images
+        .movedList()
+        .map(image => property.images().create({ path: image.fileName}))
+    )
+  }
+
+  async show ({ request, params, response }){
+    return response.download(Helpers.tmpPath(`uploads/${params.path}`))
+  }
+
+}
+
+module.exports = ImageController
